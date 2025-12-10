38aac49ac21d24bf8a13b731744bce39a926a25e
pradeepsn10101 <pradeepsn10101@gmail.com>
2025-12-10 13:10:07 +0530

    asdf

--- app/index.js
+++ app/index.js
@@ -13,6 +13,11 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ message: "API is working!", status: "success" }));
     }
+    else if (req.url === "/health") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ status: "ok" }));
+}
+
     else {
         res.writeHead(404, { "Content-Type": "text/plain" });
         res.end("Not Found");
